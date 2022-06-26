bb77db0029a1bef30481cd33390d7592bc3c7bcf
Mandeep Kaur Chahal <99150991+mkchahal@users.noreply.github.com>
2022-06-25 20:51:28 -0700

    Completed the styling of Sidebar component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.scss';
+import Sidebar from './components/Sidebar/Sidebar';
 // import { BrowserRouter as Router, Route} from "react-router-dom";
 
 function App() {
   return (
     <div className='app'>
-      Hello
+      <Sidebar />
     </div>
   );
 }
