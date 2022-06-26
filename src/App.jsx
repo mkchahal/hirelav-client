bccd1044618945f7093b866707a30b1e40f7cd17
Mandeep Kaur Chahal <99150991+mkchahal@users.noreply.github.com>
2022-06-25 19:28:38 -0700

    Added partials styles and added the style for main page.

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.scss';
+// import { BrowserRouter as Router, Route} from "react-router-dom";
+
+function App() {
+  return (
+    <div className='app'>
+      Hello
+    </div>
+  );
+}
+
+export default App;
