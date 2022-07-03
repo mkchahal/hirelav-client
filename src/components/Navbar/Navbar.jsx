2e20a2ff1c38628a113249f35448fc4b33f86ba0
Mandeep Kaur Chahal <99150991+mkchahal@users.noreply.github.com>
2022-07-03 14:07:48 -0700

    Revised the NavBar to add highlighted link to Login and Signup.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,6 +4,7 @@ import logo from '../../assets/logos/hirelav.png';
 export default function Navbar() {
 
     const [nav, setNav] = useState(false);
+    const path = window.location.pathname.slice(1);
 
     const changeBackground = () => {
         if(window.scrollY >= 50) { 
@@ -25,12 +26,12 @@ export default function Navbar() {
             <span className='nav-icon'></span>
         </label>
         <ul className="menu">
-            <li><a href="/" className='active'>Home</a></li>
+            <li><a href="/" className={path === '' ? 'active' : ''}>Home</a></li>
             <li><a href="/">About</a></li>
             <li><a href="/">Job Openings</a></li>
             <li><a href="/">Contact</a></li>
-            <li><a href="/login">Log In</a></li>
-            <li><a href="/register">Sign Up</a></li>
+            <li><a href="/login" className={path === 'login' ? 'active' : ''}>Log In</a></li>
+            <li><a href="/register" className={path === 'register' ? 'active' : ''}>Sign Up</a></li>
         </ul>
     </nav>
   )
