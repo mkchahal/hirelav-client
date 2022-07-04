d692840a503156641419baab436626ac6e3fefd8
Mandeep Kaur Chahal <99150991+mkchahal@users.noreply.github.com>
2022-07-03 17:22:23 -0700

    Formatted the table and created initial styling of.

--- src/components/AppTable/AppTable.jsx
+++ src/components/AppTable/AppTable.jsx
@@ -1,92 +1,128 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
-import Table from 'react-bootstrap/Table';
-import { Dropdown, Button } from 'semantic-ui-react';
-import { APPLICATIONS_URL, getAllApplications, getAllJobs } from '../../utils/APIUtils';
-
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import './AppTable.scss';
+import {
+    APPLICATIONS_URL,
+    getAllApplications,
+    getAllJobs,
+} from "../../utils/APIUtils";
+import { Button, Checkbox, Dropdown, Icon, Menu, Table } from "semantic-ui-react";
 
 export default function AppTable() {
     const [apps, setApps] = useState([]);
     const [jobs, setJobs] = useState([]);
 
     useEffect(() => {
-        let token = sessionStorage.getItem('authToken');
+        let token = sessionStorage.getItem("authToken");
         getAllApplications(token, setApps);
         getAllJobs(token, setJobs);
     }, []);
 
-    const handleDelete = id => {
-        let token = sessionStorage.getItem('authToken');
-        axios.delete(`${APPLICATIONS_URL}/${id}`, {
-            headers: {
-                Authorization: `Bearer ${token}`
-            },
-        })
+    const handleDelete = (id) => {
+        let token = sessionStorage.getItem("authToken");
+        axios
+            .delete(`${APPLICATIONS_URL}/${id}`, {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            })
             .then(() => getAllApplications(token, setApps))
-            .catch(err => console.log(err))
-    }
+            .catch((err) => console.log(err));
+    };
 
     const handleStatusChange = (event, id) => {
-        let token = sessionStorage.getItem('authToken');
+        let token = sessionStorage.getItem("authToken");
         console.log(event.target.innerText);
-        axios.put(`${APPLICATIONS_URL}/${id}`, {status: event.target.innerText}, {
-            headers: {
-                Authorization: `Bearer ${token}`
-            }
-        })
+        axios
+            .put(
+                `${APPLICATIONS_URL}/${id}`,
+                { status: event.target.innerText },
+                {
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                    },
+                }
+            )
             .then(() => getAllApplications(token, setApps))
-            .catch(err => console.log(err.response.data))
-    }
+            .catch((err) => console.log(err.response.data));
+    };
 
     return (
-        <>
-            <Table striped bordered hover>
-                <thead>
-                    <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">Name</th>
-                        <th scope="col">Job</th>
-                        <th scope="col">Status</th>
-                        <th scope="col">Email</th>
-                        <th scope="col">Phone</th>
-                        <th scope="col">Applied On</th>
-                        <th scope="col">Actions</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        apps.map(app =>
-                            <tr key={app.id}>
-                                <th scope="row">{app.id}</th>
-                                <td>{app.firstName} {app.lastName}</td>
-                                <td>{jobs[app.job_id].title}</td>
-                                <td>
-                                    <Dropdown
-                                        defaultValue={app.status}
-                                        fluid
-                                        selection
-                                        options={[
-                                            { value: 'Applied', text: 'Applied' },
-                                            { value: 'Shortlisted', text: 'Shortlisted' },
-                                            { value: 'Interview', text: 'Interview' },
-                                            { value: 'Coding Challenge', text: 'Coding Challenge' },
-                                            { value: 'Offer', text: 'Offer' },
-                                            { value: 'Hired', text: 'Hired' },
-                                        ]}
-                                        onChange={(event) => handleStatusChange(event, app.id)}
-                                    />
-                                </td>
-                                <td>{app.email}</td>
-                                <td>{app.phone}</td>
-                                <td>{app.updated_at.slice(0, 10)}</td>
-                                <td>
-                                    <Button onClick={() => handleDelete(app.id)} icon='delete' color='red' compact />
-                                </td>
-                            </tr>
-                        )
-                    }
-                </tbody>
+        <div className="app-table__wrapper">
+            <Table celled selectable color='violet'>
+                <Table.Header>
+                    <Table.Row>
+                        <Table.HeaderCell>Name</Table.HeaderCell>
+                        <Table.HeaderCell>Job</Table.HeaderCell>
+                        <Table.HeaderCell>Status</Table.HeaderCell>
+                        <Table.HeaderCell>Email</Table.HeaderCell>
+                        <Table.HeaderCell>Phone</Table.HeaderCell>
+                        <Table.HeaderCell>Applied On</Table.HeaderCell>
+                        <Table.HeaderCell>Actions</Table.HeaderCell>
+                    </Table.Row>
+                </Table.Header>
+
+                {apps.map(app => (
+                    <Table.Body>
+                        <Table.Row>
+                            <Table.Cell>{app.firstName} {app.lastName}</Table.Cell>
+                            <Table.Cell>{app.job_id}</Table.Cell>
+                            <Table.Cell>
+                                <Dropdown text={app.status}
+                                    fluid
+                                    options={[
+                                        { value: "Applied", text: "Applied" },
+                                        { value: "Shortlisted", text: "Shortlisted" },
+                                        { value: "Interview", text: "Interview" },
+                                        { value: "Coding Challenge", text: "Coding Challenge" },
+                                        { value: "Offer", text: "Offer" },
+                                        { value: "Hired", text: "Hired" },
+                                    ]}
+                                    onChange={(event) => handleStatusChange(event, app.id)}
+                                />
+                            </Table.Cell>
+                            <Table.Cell>{app.email}</Table.Cell>
+                            <Table.Cell>{app.phone}</Table.Cell>
+                            <Table.Cell>{app.updated_at.slice(0, 10)}</Table.Cell>
+                            <Table.Cell>
+                                <Button
+                                    onClick={() => handleDelete(app.id)}
+                                    icon="delete"
+                                    color="red"
+                                    compact
+                                />
+                            </Table.Cell>
+                        </Table.Row>
+                    </Table.Body>
+                ))}
+
+                <Table.Footer>
+                    <Table.Row>
+                        <Table.HeaderCell colSpan="7">
+                            <Button
+                                icon
+                                labelPosition="left"
+                                color="violet"
+                                size="small"
+                            >
+                                <Icon name="add" /> Add Application Manually
+                            </Button>
+                            <Menu floated='right' pagination>
+                                <Menu.Item as='a' icon>
+                                    <Icon name='chevron left' />
+                                </Menu.Item>
+                                <Menu.Item as='a'>1</Menu.Item>
+                                <Menu.Item as='a'>2</Menu.Item>
+                                <Menu.Item as='a'>3</Menu.Item>
+                                <Menu.Item as='a'>4</Menu.Item>
+                                <Menu.Item as='a' icon>
+                                    <Icon name='chevron right' />
+                                </Menu.Item>
+                            </Menu>
+                        </Table.HeaderCell>
+                    </Table.Row>
+                </Table.Footer>
             </Table>
-        </>
-    )
+        </div>
+    );
 }
