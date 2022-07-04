b0489ff343df675524ec0f82766b15d0502dd36a
Mandeep Kaur Chahal <99150991+mkchahal@users.noreply.github.com>
2022-07-03 22:17:44 -0700

    Added the active class to profile header.

--- src/components/ProfileHeader/ProfileHeader.jsx
+++ src/components/ProfileHeader/ProfileHeader.jsx
@@ -1,28 +1,38 @@
-import React, { Component } from 'react'
+import React, { Component, useState } from 'react'
 import { Icon, Search } from 'semantic-ui-react';
+import logo from '../../assets/logos/hirelav.png';
 import './ProfileHeader.scss';
 
-export default class ProfileHeader extends Component {
+export default function ProfileHeader({ view, setView }) {
 
-  render() {
+  const [nav, setNav] = useState(false);
 
-    return (
-      <div className='profile-nav'>
-        <div className="profile-nav__left">
-          <p>HireLav</p>
-          <button onClick={() => {
-            this.props.setView(!this.props.view)
-          }} className='profile-nav__mini'><Icon name='bars' size='large'/></button>
-          <Search input={{ icon: 'search', iconPosition: 'left' }} />
-        </div>
-        <div className="profile-nav__right">
-          <button className='profile-nav__mini'><Icon name='bell outline' size='large'/></button>
-          <button className='profile-nav__user'>
-            <img src='https://t3.ftcdn.net/jpg/02/52/62/04/360_F_252620498_MTwvbrwAtAnrj0dGiNrxXsfUTdf9w5E8.jpg' alt="user" />
-            <Icon name='setting' size='large'/>
-          </button>
-        </div>
-      </div>
-    )
+  const changeBackground = () => {
+    if (window.scrollY >= 50) {
+      setNav(true);
+    } else {
+      setNav(false);
+    }
   }
+
+  window.addEventListener('scroll', changeBackground);
+
+  return (
+    <div className={nav ? 'profile-nav__active' : 'profile-nav'}>
+      <div className="profile-nav__left">
+        <img src={logo} alt="logo" />
+        <button onClick={() => {
+          setView(!view)
+        }} className='profile-nav__mini'><Icon name='bars' size='large' /></button>
+        <Search input={{ icon: 'search', iconPosition: 'left' }} />
+      </div>
+      <div className="profile-nav__right">
+        <button className='profile-nav__mini'><Icon name='bell outline' size='large' /></button>
+        <button className='profile-nav__user'>
+          <img src='https://t3.ftcdn.net/jpg/02/52/62/04/360_F_252620498_MTwvbrwAtAnrj0dGiNrxXsfUTdf9w5E8.jpg' alt="user" />
+          <Icon name='setting' size='large' />
+        </button>
+      </div>
+    </div>
+  )
 }
--- src/components/ProfileNav/ProfileNav.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Navbar } from 'react-bootstrap';
-
-export function ProfileNav() {
-  return <Navbar bg="light" expand="lg">
-      </Navbar>;
-}
-  
