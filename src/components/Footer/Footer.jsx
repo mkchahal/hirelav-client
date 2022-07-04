6b7dffc7057ffa254d893e41f6ba003526130fc9
Mandeep Kaur Chahal <99150991+mkchahal@users.noreply.github.com>
2022-07-04 12:42:59 -0700

    Added contact details to the footer of the page.

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,43 @@
 import React from 'react';
 import './Footer.scss';
+import { FaGithub, FaLinkedinIn } from 'react-icons/fa';
+import { FiMail } from 'react-icons/fi';
 
 function Footer() {
   return (
     <div className='footer'>
-        <p className='footer__text'>© 2022, made with ♥ by <strong>Mandeep K Chahal</strong>.</p>
+      <div class='footer__contact'>
+        <div className='footer__contact--social'>
+          <p>Conect with me: </p>
+          <ul>
+            <li className='footer__icons'>
+              <a href='https://github.com/mkchahal' rel='noreferrer' target='_blank'>
+                <FaGithub />
+              </a>
+            </li>
+            <li className='footer__icons'>
+              <a href='"mailto:chahal.mandeep@outlook.com"' rel='noreferrer' target='_blank'>
+                <FiMail />
+              </a>
+            </li>
+            <li className='footer__icons'>
+              <a href='https://www.linkedin.com/in/mkchahal' rel='noreferrer' target='_blank'>
+                <FaLinkedinIn />
+              </a>
+            </li>
+          </ul>
+        </div>
+
+        <div className='footer__contact--address'>
+          <p>I ❤️ handwritten letters, write to me at: </p>
+          <address>
+            8256 18th Avenue, <br />
+            Burnaby BC <br />
+            V3N 1K3
+          </address>
+        </div>
+        </div>
+      <p className='footer__text'>© 2022, made with ♥ by <strong>Mandeep K Chahal</strong>.</p>
     </div>
   )
 }
